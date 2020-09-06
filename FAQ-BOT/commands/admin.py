b584d1b42547de882fdbd150304b5e7386368b2c
Vusal Ismayilov <69302858+woosal1337@users.noreply.github.com>
2020-09-06 14:10:23 +0300

    2.0

--- FAQ-BOT/commands/admin.py
+++ FAQ-BOT/commands/admin.py
@@ -1,6 +1,5 @@
 import discord
 from discord.ext import commands
-import time
 
 class adminCommand(commands.Cog):
     def __init__(self, bot):
@@ -20,35 +19,6 @@ class adminCommand(commands.Cog):
 
         except ImportError as e:
             print(e)
-
-    @commands.command()
-    async def clean(self, message, value:int):
-        min = 0
-        max = 100
-
-        if(message.author.guild_permissions.administrator):
-            if(value > min and value <= max):
-                channel = message.channel
-                messages = await channel.history(limit = value).flatten()
-
-                sendMessage = await message.send("{} messages were found".format(value))
-                time.sleep(1)
-
-                await sendMessage.edit(content="{} messages are being deleted!".format(value))
-                time.sleep(0.5)
-
-                await channel.delete_messages(messages)
-                sendMessage.edit(content=":white_check_mark: {} messages were deleted! :white_check_mark:".format(value))
-                time.sleep(1)
-
-                lastMessage = channel.history(limit=1).flatten()
-                await channel.delete_messages(lastMessage)
-
-            else:
-                await message.send(":x: Message amount should be between {} and {}!".format(min, max))
-        else:
-            await message.send(":x: What da Fuq you tryin' to do biach? :x:")
-
 
 def setup(bot):
     bot.add_cog(adminCommand(bot))
